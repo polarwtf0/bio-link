2d6fe0ec18fa10d6c76b2144ccd2f4afb796369c
serenity <polar.wtf0@gmail.com>
2026-08-02 05:17:47 +0000

    Updated bio-link

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
-kofiWidgetOverlay.draw('polarwtf', {
+kofiWidgetOverlay.draw('serenityjs', {
     'type': 'floating-chat',
     'floating-chat.donateButton.text': 'Support me',
-    'floating-chat.donateButton.background-color': 'rgba(0, 0, 0, 0.25)',
-    'floating-chat.donateButton.text-color': '#ffffff',
-    'floating-chat.donateButton.backdrop-filter': 'blur(15px)',
-    'floating-chat.donateButton.box-shadow': '0 1px 2px 0 #ffffff40'
-});
+    'floating-chat.donateButton.background-color': '#323842',
+    'floating-chat.donateButton.text-color': '#fff'
+  });
 
-const text = "@polar.wtf"
+const text = "@serenity.js"
 const zeroWidthSpace = "\u200B"
 let typewriter = document.getElementById("typewriter")
 let i = 0;
